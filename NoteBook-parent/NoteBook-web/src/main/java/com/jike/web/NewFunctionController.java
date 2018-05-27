a2c9e5f6a125f6fd5cd1148bac1c3d0de2892e22
ZGHS <1768702303@qq.com>
2018-05-27 21:22:54 +0800

    aa

--- NoteBook-parent/NoteBook-web/src/main/java/com/jike/web/NewFunctionController.java
+++ NoteBook-parent/NoteBook-web/src/main/java/com/jike/web/NewFunctionController.java
@@ -14,27 +14,39 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.alibaba.fastjson.JSON;
 import com.jike.entity.RecordInfo;
 import com.jike.entity.UserInfo;
+import com.jike.service.NewFunctionService;
 import com.jike.service.RecordInfoService;
+import com.jike.utils.AnalyseUtil;
 
 @Controller
 public class NewFunctionController {
 	@Autowired
 	private RecordInfoService recordInfoService;
+	@Autowired
+	private NewFunctionService newFunctionService;
 
 	@RequestMapping(value = "sendNews", produces = "text/html;charset=UTF-8")
 	@ResponseBody
 	public String sendNews(HttpServletRequest request, HttpSession session, HttpServletResponse response) {
 		// 跨域问题，需要加上
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		Integer uId = ((UserInfo) (session.getAttribute("accountInfo"))).getuId();
-
-		UserInfo userInfo = new UserInfo();
-		userInfo.setuId(uId);
-		List<RecordInfo> selectNewestOne = recordInfoService.selectNewestOne(userInfo);
-
-		String keyContent = selectNewestOne.get(0).getrContent();
-
-		return "{\"info\":\"" + keyContent + "\"}";
+		// Integer uId = ((UserInfo) (session.getAttribute("accountInfo"))).getuId();
+		String uId = request.getParameter("uId");
+		// Integer uId =1;
+		//
+		// UserInfo userInfo = new UserInfo();
+		// userInfo.setuId(Integer.parseInt(uId));
+		List<RecordInfo> byUid = recordInfoService.getByUid(Integer.parseInt(uId));
+		// List<RecordInfo> selectNewestOne =
+		// recordInfoService.selectNewestOne(userInfo);
+		//
+		// String keyContent = selectNewestOne.get(0).getrContent();
+		// String text = "北京是一家高科技公司";
+		String text = byUid.get(byUid.size() - 1).getrContent();
+		// String analyseResult = AnalyseUtil.getAnalyseResult(text);
+		// System.out.println("希望可以出来结果+++++++++++++++++"+analyseResult);
+		List<String> news = newFunctionService.getNews(text);
+		return news.get(0);
 	}
 
 	public RecordInfoService getRecordInfoService() {
--- NoteBook-parent/NoteBook-web/src/main/java/com/jike/web/UserInfoController.java
+++ NoteBook-parent/NoteBook-web/src/main/java/com/jike/web/UserInfoController.java
@@ -24,22 +24,24 @@ public class UserInfoController {
 		UserInfo register = userInfoService.register(userInfo);
 		String jsonString = JSON.toJSONString(register);
 		System.out.println("registerAccount run");
+		System.out.println("jsonString::"+jsonString);
 		return jsonString;
 	}
+
 	@RequestMapping(value = "register", produces = "text/html;charset=UTF-8")
 	@ResponseBody
-	public String registerAccount(HttpServletRequest request,HttpServletResponse response,HttpSession session) {
-		//跨域问题，需要加上
-				response.setHeader("Access-Control-Allow-Origin", "*");
-				String uAccount = request.getParameter("uAccount");
-				String uPassword = request.getParameter("uPassword");
-				UserInfo userInfo=new UserInfo(uAccount,uPassword);
-				UserInfo register = userInfoService.register(userInfo);
-				if (register!=null) {
-					session.setAttribute("accountInfo", register);
-					return "{\"info\":\"success\",\"page\":\"login.html\"}";
-				}
-				return "{\"info\":\"failure\",\"page\":\"register.html\"}";
+	public String registerAccount(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+		// 跨域问题，需要加上
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		String uAccount = request.getParameter("uAccount");
+		String uPassword = request.getParameter("uPassword");
+		UserInfo userInfo = new UserInfo(uAccount, uPassword);
+		UserInfo register = userInfoService.register(userInfo);
+		if (register != null) {
+			session.setAttribute("accountInfo", register);
+			return "{\"info\":\"success\",\"page\":\"login.html\"}";
+		}
+		return "{\"info\":\"failure\",\"page\":\"register.html\"}";
 	}
 
 	@RequestMapping(value = "modifyAndroid", produces = "text/html;charset=UTF-8")
@@ -47,20 +49,21 @@ public class UserInfoController {
 	public String modifyUserInfoAndroid(UserInfo userInfo) {
 		UserInfo modifyProfile = userInfoService.modifyProfile(userInfo);
 		String jsonString = JSON.toJSONString(modifyProfile);
-		System.out.println("modifyUserInfo run");
+		System.out.println("modifyUserInfo run  "+jsonString);
 		return jsonString;
 	}
-//我已经改了不要动
+
+	// 我已经改了不要动
 	@RequestMapping(value = "modify", produces = "text/html;charset=UTF-8")
 	@ResponseBody
-	public String modifyUserInfo(HttpServletRequest request,HttpServletResponse response,HttpSession session) {
+	public String modifyUserInfo(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		String uName = request.getParameter("uName");
 		String uPassword = request.getParameter("uPassword");
 		UserInfo attribute = (UserInfo) session.getAttribute("accountInfo");
 		Integer uId = attribute.getuId();
 		String uAccount = attribute.getuAccount();
-		UserInfo userInfo=new UserInfo(uId,uName,uAccount,uPassword);
+		UserInfo userInfo = new UserInfo(uId, uName, uAccount, uPassword);
 		UserInfo modifyProfile = userInfoService.modifyProfile(userInfo);
 		session.invalidate();
 		return "{\"page\":\"index.action\"}";
@@ -68,13 +71,13 @@ public class UserInfoController {
 
 	@RequestMapping(value = "login", produces = "text/html;charset=UTF-8")
 	@ResponseBody
-	public String login(HttpSession session,HttpServletRequest request,HttpServletResponse response) {
-		//跨域问题，需要加上
+	public String login(HttpSession session, HttpServletRequest request, HttpServletResponse response) {
+		// 跨域问题，需要加上
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		String uAccount = request.getParameter("uAccount");
 		String uPassword = request.getParameter("uPassword");
-		UserInfo userInfo=new UserInfo(uAccount,uPassword);
-		
+		UserInfo userInfo = new UserInfo(uAccount, uPassword);
+
 		UserInfo login = userInfoService.login(userInfo);
 		if (login != null) {
 			session.setAttribute("accountInfo", login);
@@ -82,43 +85,40 @@ public class UserInfoController {
 		} else
 			return "{\"page\":\"login.html\"}";
 	}
-	
-	
+
 	@RequestMapping(value = "isLogin", produces = "text/html;charset=UTF-8")
 	@ResponseBody
-	public String isLogin(HttpSession session,HttpServletResponse response) {
+	public String isLogin(HttpSession session, HttpServletResponse response) {
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		if (session.getAttribute("accountInfo")==null) {
+		if (session.getAttribute("accountInfo") == null) {
 			return "{\"info\":\"failure\",\"page\":\"login.html\"}";
-		}else {
+		} else {
 			return "{\"info\":\"success\",\"page\":\"index.action\"}";
 		}
 	}
-	
+
 	@RequestMapping(value = "index", produces = "text/html;charset=UTF-8")
 	public String loginSuccess(HttpSession session) {
-			return "index";
+		return "index";
 	}
 
-	
-	
 	@RequestMapping(value = "regetsession", produces = "text/html;charset=UTF-8")
 	@ResponseBody
 	public String reGetSession(HttpSession session) {
-		//HttpSession session = request.getSession();
-		UserInfo getsession = (UserInfo) session.getAttribute("accountInfo");
-		String jsonString = JSON.toJSONString(getsession);
+		UserInfo userInfo = (UserInfo) session.getAttribute("accountInfo");
+		UserInfo login = userInfoService.login(userInfo);
+		session.setAttribute("accountInfo", login);
+		String jsonString = JSON.toJSONString(login);
 		return jsonString;
 	}
-
 
 	@RequestMapping(value = "logout", produces = "text/html;charset=UTF-8")
 	@ResponseBody
 	public String logOut(HttpSession session) {
-	
-			session.invalidate();
-			return "{\"page\":\"login.html\"}";
-		
+
+		session.invalidate();
+		return "{\"page\":\"login.html\"}";
+
 	}
 
 	// for text
@@ -131,18 +131,17 @@ public class UserInfoController {
 	@ResponseBody
 	public String loginAndroid(UserInfo userInfo, HttpSession session) {
 		UserInfo login = userInfoService.login(userInfo);
-		System.out.println("张港华++++++++Run"+userInfo.toString());
-		
+		System.out.println("张港华++++++++Run" + userInfo.toString());
+
 		if (login != null) {
 			session.setAttribute("accountInfoAndroid", login);
 			Object attribute = session.getAttribute("accountInfoAndroid");
-			System.out.println("accountInfoAndroid Run:->>>>>>>>>>>>>>"+attribute.toString());
+			System.out.println("accountInfoAndroid Run:->>>>>>>>>>>>>>" + attribute.toString());
 			String jsonString = JSON.toJSONString(login);
 			return jsonString;
 		} else
 			return "";
-		
-		
+
 	}
 
 	public UserInfoService getUserInfoService() {
